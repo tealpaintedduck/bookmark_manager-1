7d4b5310faadd99b701ab5a41c152bb230862125
Diego Regules <diegoregules@gmail.com>
2015-08-12 10:21:00 +0100

    small changes to refactor

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -7,8 +7,9 @@ class BookMarkManager < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
 
   get '/' do
-    redirect to('/links')
+    redirect '/links'
   end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
@@ -21,11 +22,9 @@ class BookMarkManager < Sinatra::Base
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
     tag = Tag.create(name: params[:tag])
-    p 'TAG in LINKS '
-    p tag
     link.tags << tag
     link.save
-    redirect to('/links') #redirect '/links'
+    redirect '/links'
   end
 
   get '/tags/:name' do
